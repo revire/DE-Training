d0811338dfa2041d6d1cbe33ae3fb14b245365a9
revire <riva.revekka@gmail.com>
2021-02-11 22:34:31 +0300

    вуздщн ашч

--- task_aws/deploy.sh
+++ task_aws/deploy.sh
@@ -1,4 +1,4 @@
 mkdir -p lambda_layers/python/lib/python3.8/site-packages
 sudo pip install -r requirments.txt -t lambda_layers/python/lib/python3.8/site-packages/
 cd lambda_layers && zip -r lambda_layer.zip python
-cd lambda_tf && terraform apply -auto-approve -lock=false
+cd ../lambda_tf && terraform apply -auto-approve -lock=false
